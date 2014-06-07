70cdbb67f2e16ac463f2653f5afbfc969e878066
Georgiy Sosnovskiy <sosnovskiy.g@gmail.com>
2014-06-07 23:19:25 +0400

    Добавил строку

--- public/index.php
+++ public/index.php
@@ -1,3 +1,19 @@
-<?php
-	echo 'Hello Dude!!!';
-?>
+<html>
+<head>
+</head>
+
+<body>
+	<div id="body">
+		<h1>
+		<?php
+			echo 'Hello Dude!!!';
+		?>
+		</h1>
+		<h2>
+		<?php
+			echo 'Second Commit';
+		?>
+		</h2>
+	</div>
+</body>	
+<html>
